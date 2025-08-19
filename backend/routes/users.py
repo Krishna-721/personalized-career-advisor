23e1f06fa63735e4d9b7f9caaf0c112f6c1c3323
Krishna-721 <gourwarvamshi@gmaol.com>
2025-08-19 13:36:09 +0530

    Updated users with users to skill mapping

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -7,6 +7,6 @@ users_bp = Blueprint('users', __name__)  # Users Blueprint
 def list_users():
     return jsonify(get_all_users())
 
-@users_bp.route('/users/<int:user_id>', methods=['GET'])
-def user_skills(user_id):
-    return jsonify(get_user_skills(user_id))
+@users_bp.route('/users/<int:user_id>/skills/<int: skill_id', methods=['GET'])
+def user_skills(user_id,skill_id):
+    return jsonify(get_user_skills(user_id,skill_id))
